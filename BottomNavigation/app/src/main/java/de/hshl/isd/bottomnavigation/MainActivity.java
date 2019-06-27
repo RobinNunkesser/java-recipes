cdbfa57b85e59542bd35be1eb53355e0e5008e9d
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-06-27 15:06:17 +0200

    New recipe: BottomNavigation

---
+++ BottomNavigation/app/src/main/java/de/hshl/isd/bottomnavigation/MainActivity.java
@@ -0,0 +1,28 @@
+package de.hshl.isd.bottomnavigation;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController mNavController;
+    private BottomNavigationView mNavView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNavController =
+                Navigation.findNavController(this, R.id.nav_host_fragment);
+        mNavView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(mNavView, mNavController);
+
+    }
+
+}
