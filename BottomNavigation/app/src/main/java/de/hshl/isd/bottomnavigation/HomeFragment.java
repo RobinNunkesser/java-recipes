dfcabaaeef67bbd6deb1d7acca64689cbb61c998
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-21 13:09:08 +0100

    Gradle updates

--- BottomNavigation/app/src/main/java/de/hshl/isd/bottomnavigation/HomeFragment.java
+++ BottomNavigation/app/src/main/java/de/hshl/isd/bottomnavigation/HomeFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -15,6 +17,8 @@ import androidx.navigation.Navigation;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private Button mNavButton;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -30,20 +34,18 @@ public class HomeFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        getActivity().findViewById(R.id.navButton).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Bundle bundle = new Bundle();
-                        bundle.putString(SecondLevelFragment.ARG_PARAM1,
-                                "value1");
-                        bundle.putString(SecondLevelFragment.ARG_PARAM2,
-                                "value2");
-                        Navigation.findNavController(getActivity(),
-                                R.id.nav_host_fragment).navigate(
-                                R.id.action_homeFragment_to_secondLevelFragment,
-                                bundle);
-                    }
+        mNavButton = (Button) requireActivity().findViewById(R.id.navButton);
+        mNavButton.setOnClickListener(
+                v -> {
+                    Bundle bundle = new Bundle();
+                    bundle.putString(SecondLevelFragment.ARG_PARAM1,
+                            "value1");
+                    bundle.putString(SecondLevelFragment.ARG_PARAM2,
+                            "value2");
+                    Navigation.findNavController(requireActivity(),
+                            R.id.nav_host_fragment).navigate(
+                            R.id.action_homeFragment_to_secondLevelFragment,
+                            bundle);
                 });
     }
 
